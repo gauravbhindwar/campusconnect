c67bca57fc3bd53aeb951fab3e33cb09535f1ab6
gauravbhindwar <bhindwargaurav@gmail.com>
2025-04-21 23:37:52 +0530

    fix: Comment out bottom wave divider SVG in HeaderSection

--- src/components/Landing/HeaderSection.tsx
+++ src/components/Landing/HeaderSection.tsx
@@ -256,11 +256,11 @@ const HeaderSection = () => {
       </div>
       
       {/* Bottom wave divider */}
-      <div className="absolute bottom-0 left-0 w-full">
+      {/* <div className="absolute bottom-0 left-0 w-full">
         <svg className="w-full h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120">
           <path fill="#ffffff" fillOpacity="1" d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,80C672,64,768,64,864,74.7C960,85,1056,107,1152,101.3C1248,96,1344,64,1392,48L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
-      </div>
+      </div> */}
     </header>
   );
 };
